630d473bd18cc4815809cb1ad2914f01fd0efab3
vmdharan <rexar7@gmail.com>
2016-02-06 14:54:52 +1100

    Added Insertion sort algorithm.
    Added the Insertion sort algorithm to the project.

---
+++ src/InsertionSort.py
@@ -0,0 +1,21 @@
+# Implementation of Insertion sort algorithm  for sorting
+class InsertionSort:
+
+    # Sort input array
+    def sort(A):
+        a_len = len(A)
+
+        # Iterater over the array
+        for i in range(1, a_len):
+            v = A[i]
+            j = i - 1
+            # While index is positive and element is greater than A[i] 
+            # scan and insert A[i] into its proper position.
+            while((j >= 0) and A[j] > v):
+                A[j+1] = A[j]
+                j = j - 1
+            A[j+1] = v
+
+        # Return the sorted array
+        return A
+        
