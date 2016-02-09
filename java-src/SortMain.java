e53e52152a55b6cd836b94c241a06d0667507a02
vmdharan <rexar7@gmail.com>
2016-02-09 21:30:49 +1100

    Started adding Java code for algorithms.
    - Renamed the existing source directory from 'src' to 'python-src'.
    - Added a source directory 'java-src' for implementation of the
    algorithms in Java.
    - Added a main class 'SortMain.java' for invoking the sorting methods.
    - Added a batch file to compile.

---
+++ java-src/SortMain.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+// Main class for the running the sorting algorithms.
+public class SortMain
+{
+    public static void main(String args[])
+    {
+        int testArray[] = { 4, 2, 1, 7, 3, 6, 5, 9, 0, 8, 7, 4, 3, 10 };
+        
+        // Print out test array.
+        System.out.println(Arrays.toString(testArray));
+        
+        // Print out sorted array.
+    }
+}
