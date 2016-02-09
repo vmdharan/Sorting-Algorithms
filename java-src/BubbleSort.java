cd28c9418056f1630a5c4e1ba342cab4a69785a6
vmdharan <rexar7@gmail.com>
2016-02-09 21:56:43 +1100

    Added the BubbleSort algorithm in java.
    - Added the BubbleSort algorithm in java.
    - Updated the SortMain class to print out the sorted data.

---
+++ java-src/BubbleSort.java
@@ -0,0 +1,41 @@
+// Bubblesort algorithm
+public class BubbleSort
+{
+    private int[] A;
+    
+    // Constructor
+    public BubbleSort(int[] data)
+    {
+        A = data;
+    }
+    
+    public int[] sort()
+    {
+        int i, j;
+        
+        // Compare adjacent elements of the array and exchange the 
+        // values if they are out of order.
+        for(i=0; i<A.length-1; i++)
+        {
+            for(j=0; j<A.length-1; j++)
+            {
+                if(A[j+1] < A[j])
+                {
+                    swap(A, j, j+1);
+                }
+            }
+        }
+        
+        return A;
+    }
+    
+    // Swap array elements at A[u] and A[v].
+    private void swap(int[] A, int u, int v)
+    {
+        int temp;
+        
+        temp = A[u];
+        A[u] = A[v];
+        A[v] = temp;
+    }
+}
--- java-src/SortMain.java
+++ java-src/SortMain.java
@@ -8,8 +8,13 @@ public class SortMain
         int testArray[] = { 4, 2, 1, 7, 3, 6, 5, 9, 0, 8, 7, 4, 3, 10 };
         
         // Print out test array.
+        System.out.println("Original data:");
         System.out.println(Arrays.toString(testArray));
+        System.out.println("\n");
         
         // Print out sorted array.
+        BubbleSort bs1 = new BubbleSort(testArray);
+        System.out.println("Sorting using BubbleSort:");
+        System.out.println(Arrays.toString(bs1.sort()));
     }
 }
